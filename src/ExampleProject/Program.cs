df8aa16c4a7b7bf655f0b819a3b40cc16b44c667
Christopher F <foxbot@protonmail.com>
2016-10-14 18:13:22 -0400

    Add example project; beta1

---
+++ src/ExampleProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Net.WebSockets;
+using Discord.Addons.WS4NetCompatibility;
+using System.Threading.Tasks;
+using System;
+
+namespace ExampleProject
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            new Program().Run().GetAwaiter().GetResult();
+
+        public async Task Run()
+        {
+            var client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                WebSocketProvider = new WebSocketProvider(() => new WS4NetProvider()),
+                LogLevel = LogSeverity.Debug
+            });
+
+            var token = Environment.GetEnvironmentVariable("discord-foxboat-token");
+            client.Log += msg =>
+            {
+                Console.WriteLine(msg.ToString());
+                return Task.CompletedTask;
+            };
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
